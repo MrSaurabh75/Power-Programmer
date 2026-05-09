6e14eb71240ca70c0b2f98eef53fca66b5d90aa0
Saurabh Chorge <saurabhdc7575@gmail.com>
2026-05-09 20:06:28 +0530

    9 - Palindrome added

---
+++ Easy Programs/Palindrome9.py
@@ -0,0 +1,36 @@
+# 9. Palindrome Number
+
+# Hint
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# Example 2:
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# Example 3:
+
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+# Answer:
+
+class Solution(object):
+    def isPalindrome(self, x):
+        newX = x
+        rev = ""
+        if x < 0:
+            return False
+        while x!=0:
+            digit = x%10
+            rev += str(digit)
+            x//=10
+        return int(rev) == newX
