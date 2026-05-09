61f3258386767f5ceec0b07cfe54b4227e83cfa3
Saurabh Chorge <saurabhdc7575@gmail.com>
2026-05-09 19:15:05 +0530

    2520 - Count and Div added

---
+++ Easy Programs/CountDiv2520.py
@@ -0,0 +1,36 @@
+# 2520. Count the Digits That Divide a Number
+
+# Hint
+# Given an integer num, return the number of digits in num that divide num.
+
+# An integer val divides nums if nums % val == 0.
+
+ 
+
+# Example 1:
+
+# Input: num = 7
+# Output: 1
+# Explanation: 7 divides itself, hence the answer is 1.
+# Example 2:
+
+# Input: num = 121
+# Output: 2
+# Explanation: 121 is divisible by 1, but not 2. Since 1 occurs twice as a digit, we return 2.
+# Example 3:
+
+# Input: num = 1248
+# Output: 4
+# Explanation: 1248 is divisible by all of its digits, hence the answer is 4.
+
+# Answer:
+class Solution(object):
+    def countDigits(self, num):
+        countDiv=0
+        newNum=num
+        while newNum!=0:
+            digit=newNum%10
+            if num%digit==0:
+                countDiv+=1
+            newNum//=10
+        return countDiv
