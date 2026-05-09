28c03517e06987f4b6f4687a13f4923d8128b222
Saurabh Chorge <saurabhdc7575@gmail.com>
2026-05-09 18:15:00 +0530

    412 - FizzBuzz added

--- Variables and DataTypes/variable.py
+++
@@ -1,8 +0,0 @@
-#Variable - It is a name that given to a memory location. It is used to store data in the memory. It can be of different data types.
-#Syntax - variable_name = value
-#Example -
-x = 10
-y = 20.5
-name = "John"
-#In the above example, x is a variable of type int, y is a variable of type float and name is a variable of type str.
-
